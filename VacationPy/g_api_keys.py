cf01cf7f433c72470a3d6b0e35629fad4ec89c45
Shadee Tabasi <shadeetabasi@gmail.com>
2021-01-03 22:41:49 -0500

    updated api keys documents

--- VacationPy/g_api_keys.py
+++ VacationPy/g_api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "100de59ea73ffbfc6adfe1c7bc7b3575"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
---
+++ WeatherPy/weather_api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "100de59ea73ffbfc6adfe1c7bc7b3575"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
